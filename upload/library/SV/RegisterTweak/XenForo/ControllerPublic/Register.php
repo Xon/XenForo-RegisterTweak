232aed1f8521e8b3bc72bb246019ce76adf1e3c9
Xon <tazipper1@hotmail.com>
2015-05-12 20:45:34 +0800

    Do not break external registration methods

--- upload/library/SV/RegisterTweak/XenForo/ControllerPublic/Register.php
+++ upload/library/SV/RegisterTweak/XenForo/ControllerPublic/Register.php
@@ -12,15 +12,4 @@ class SV_RegisterTweak_XenForo_ControllerPublic_Register extends XFCP_SV_Registe
         }
         return parent::actionRegister();
     }
-
-    public function actionValidateField()
-    {
-        if (empty(SV_RegisterTweak_Globals::$registration_permitted))
-        {
-            throw $this->responseException($this->responseError(
-                new XenForo_Phrase('new_registrations_currently_not_being_accepted')
-            ));
-        }
-        return parent::actionValidateField();
-    }
 }
