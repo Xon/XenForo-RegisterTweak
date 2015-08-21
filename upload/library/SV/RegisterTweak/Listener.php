4f943cef1660a10d8e6a35a206272a9c7a77d960
Xon <tazipper1@hotmail.com>
2015-08-21 21:49:59 +0800

    Tweak Listener

--- upload/library/SV/RegisterTweak/Listener.php
+++ upload/library/SV/RegisterTweak/Listener.php
@@ -2,10 +2,10 @@
 
 class SV_RegisterTweak_Listener
 {
-    const AddonNameSpace = 'SV_RegisterTweak';
+    const AddonNameSpace = 'SV_RegisterTweak_';
 
     public static function load_class($class, array &$extend)
     {
-        $extend[] = self::AddonNameSpace.'_'.$class;
+        $extend[] = self::AddonNameSpace.$class;
     }
 }
